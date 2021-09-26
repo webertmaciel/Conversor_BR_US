a30f6bb6d3b9ab4dec7d87c0fa36e76912024590
webert maciel <webert_maciel@hotmail.com>
2021-09-26 18:10:31 -0300

    commit 2

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
-
+let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL";
 
 function converter(){
-    let input = document.getElementById("valor")
-    let valor = input.value
+    let input = document.getElementById("valor");
+    let valor = input.value;
 
     fetch(url).then((res)=>{
         return res.json()
     }).then((data)=>{
-        console.log(data)
-        let rate = data.rate.BRL;
-        document.getElementById("resultado").innerHTML = rate.valor
+        let rate = data.USDBRL.ask;
+        document.getElementById("resultado").innerHTML = rate * valor;
     })
 }
