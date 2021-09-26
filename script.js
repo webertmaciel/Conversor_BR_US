f002174ef6d9164d7d5f4cb61505b4d5cb5f152f
webert maciel <webert_maciel@hotmail.com>
2021-09-26 18:42:13 -0300

    res3

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL";
 
+
 function converter(){
     let input = document.getElementById("valor");
     let valor = input.value;
-
-    fetch(url).then((res)=>{
-        return res.json()
-    }).then((data)=>{
+    fetch(url).then((res) => {
+    return res.json()
+    }).then((data) => {
         let rate = data.USDBRL.ask;
-        document.getElementById("resultado").innerHTML = rate * valor;
+        console.log(rate);
+        let resultado = `${valor} dolares = ${rate * valor} em reais`
+        document.getElementById("resultado").innerHTML = resultado;
     })
 }
